19fdbd84ca7d6eb7d7e8741efe26e641d502c1e8
satyasingh1655129-beep <satyasingh1655129@gmail.com>
2026-08-30 22:41:40 +0530

    Delete users.js

--- users.js
+++
@@ -1,6 +0,0 @@
-const users = [
-  { name: "doctor1", pass: "doc123", role: "doctor", groupId: "MART-001", file: "doctor.html" },
-  { name: "shop-mart", pass: "mart123", role: "pharmacist", groupId: "MART-001", file: "pharmacist.html" },
-  { name: "member101", pass: "mem123", role: "member", groupId: "MART-001", file: "member.html" },
-  { name: "service1", pass: "ser123", role: "service", groupId: "MART-001", file: "service.html" }
-];

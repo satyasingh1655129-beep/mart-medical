24a24679f607b0cf99ee5c2a39f326a82b43f99e
satyasingh1655129-beep <satyasingh1655129@gmail.com>
2026-08-30 21:50:51 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,17 +1,14 @@
-// ngcare.js - 1 file se Style + JS connect
+// cannect.js - 1 file se sab connect
 (function(){
-  // 1. Style.css connect
   var css = document.createElement('link');
   css.rel = 'stylesheet';
   css.href = 'style.css';
   document.head.appendChild(css);
 
-  // 2. QR Scanner library connect
   var qr = document.createElement('script');
   qr.src = 'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js';
   document.head.appendChild(qr);
 
-  // 3. Common.js connect
   qr.onload = function(){
     var js = document.createElement('script');
     js.src = 'common.js';
